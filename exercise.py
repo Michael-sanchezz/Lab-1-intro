308ace0f9976194bd10e49cb89612e6d510ac285
Michael <michaelsmike7@yahoo.com>
2024-01-22 13:57:56 -0500

    format strings to make it shorter

--- exercise.py
+++ exercise.py
@@ -1,37 +1,35 @@
-#Exercise 1
+# Exercise 1
+
 user_text = input("Enter some text: ")
 print(user_text)
 
-#Excercise 2
+# Excercise 2
 user_number = input("enter a number: ")
 user_int = int(user_number)
-print(user_int+1)
+print(user_int + 1)
 
-#Exercise 3
+# Exercise 3
 user_float = input("enter a number: ")
 string_to_float = float(user_float)
-print(string_to_float +.5)
+print(string_to_float + .5)
 
-#Exercise 4
+# Exercise 4
 float_1 = input("enter a number: ")
 float_2 = input("enter another number: ")
 num1 = float(float_1)
 num2 = float(float_2)
-total = num1+num2
-print(total)
+print(f'total added = {num1 + num2}')
 
-#Exercise 5
+# Exercise 5
 float_3 = input("enter a number: ")
 float_4 = input("enter another number: ")
 num3 = float(float_3)
 num4 = float(float_4)
-total_mult = num3*num4
-print(total_mult)
+print(f'total multiplied is {num3 * num4}')
 
-#Exercise 6
+# Exercise 6
 int_1 = input("enter a number: ")
 int_2 = input("enter another number: ")
 num_5 = int(int_1)
 num_6 = int(int_2)
-total_div = num_5/num_6
-print(total_div)
+print(f'total divided is {num_5 // num_6}')
