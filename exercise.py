97bf9744451f1f7b3018353c4570f4cfe0d48a92
Michael <michaelsmike7@yahoo.com>
2024-01-23 09:25:04 -0500

    printed exercise names

--- exercise.py
+++ exercise.py
@@ -1,33 +1,32 @@
-# Exercise 1
-
+Print("exercise 1")
 user_text = input("Enter some text: ")
 print(user_text)
 
-# Excercise 2
+print("exercise 2")
 user_number = input("enter a number: ")
 user_int = int(user_number)
 print(user_int + 1)
 
-# Exercise 3
+print("exercise 3")
 user_float = input("enter a number: ")
 string_to_float = float(user_float)
 print(string_to_float + .5)
 
-# Exercise 4
+print("exercise 4")
 float_1 = input("enter a number: ")
 float_2 = input("enter another number: ")
 num1 = float(float_1)
 num2 = float(float_2)
 print(f'total added = {num1 + num2}')
 
-# Exercise 5
+print("exercise 5")
 float_3 = input("enter a number: ")
 float_4 = input("enter another number: ")
 num3 = float(float_3)
 num4 = float(float_4)
 print(f'total multiplied is {num3 * num4}')
 
-# Exercise 6
+print("exercise 6")
 int_1 = input("enter a number: ")
 int_2 = input("enter another number: ")
 num_5 = int(int_1)
